3f7d773b7c0632e1fc72b966e04e2b74aa208f0b
Roger Antonio Canchanya Garcia <rcanchanya@globokas.com>
2021-10-20 13:39:36 -0500

    feat: Maquetacion base + Modulos necesarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-conversion';
+
+  currencyList: string[];
+  myForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.currencyList = [
+      "EUR",
+      "USD",
+      "GBP",
+      "JPY"
+    ];
+    this.myForm = this.formBuilder.group({
+      currencyInputCtrl: new FormControl('EUR', [Validators.required]),
+      currencyOutputCtrl: new FormControl('USD', [Validators.required]),
+      amountCtrl: new FormControl(100, [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/)]),
+      dateCtrl: new FormControl('')
+    });
+  }
 }
